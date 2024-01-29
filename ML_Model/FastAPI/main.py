e5cb10bbd16fde9f1d3fb031642ac8ac759b8f34
aravindDbhat <aravindbhat88@gmai.com>
2024-01-29 18:17:51 +0530

    "Implimentation of restnet"

--- ML_Model/FastAPI/main.py
+++ ML_Model/FastAPI/main.py
@@ -6,10 +6,113 @@ from io import BytesIO
 from PIL import Image
 import tensorflow as tf
 import cv2
+import torch
+import torch.nn as nn           # for creating  neural networks
+from torch.utils.data import DataLoader
+import torch.nn.functional as F # for functions for calculating loss
+import torchvision.transforms as transforms   # for transforming images into tensors 
+from torchvision.utils import make_grid       # for data checking
+from torchvision.datasets import ImageFolder
+from torchvision.models import resnet18# for working with classes and images
+
 
 app = FastAPI()
+class SimpleResidualBlock(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3, stride=1, padding=1)
+        self.relu1 = nn.ReLU()
+        self.conv2 = nn.Conv2d(in_channels=3, out_channels=3, kernel_size=3, stride=1, padding=1)
+        self.relu2 = nn.ReLU()
+        
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.relu1(out)
+        out = self.conv2(out)
+        return self.relu2(out) + x # ReLU can be applied before or after adding the input
+# for calculating the accuracy
+def accuracy(outputs, labels):
+    _, preds = torch.max(outputs, dim=1)
+    return torch.tensor(torch.sum(preds == labels).item() / len(preds))
 
-MODEL = tf.keras.models.load_model("ML_Model/pretrained_model/my_model.h5")
+
+# base class for the model
+class ImageClassificationBase(nn.Module):
+    
+    def training_step(self, batch):
+        images, labels = batch
+        out = self(images)                  # Generate predictions
+        loss = F.cross_entropy(out, labels) # Calculate loss
+        return loss
+    
+    def validation_step(self, batch):
+        images, labels = batch
+        out = self(images)                   # Generate prediction
+        loss = F.cross_entropy(out, labels)  # Calculate loss
+        acc = accuracy(out, labels)          # Calculate accuracy
+        return {"val_loss": loss.detach(), "val_accuracy": acc}
+    
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x["val_loss"] for x in outputs]
+        batch_accuracy = [x["val_accuracy"] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()       # Combine loss  
+        epoch_accuracy = torch.stack(batch_accuracy).mean()
+        return {"val_loss": epoch_loss, "val_accuracy": epoch_accuracy} # Combine accuracies
+    
+    def epoch_end(self, epoch, result):
+        print("Epoch [{}], last_lr: {:.5f}, train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(
+            epoch, result['lrs'][-1], result['train_loss'], result['val_loss'], result['val_accuracy']))
+        
+        
+        
+# Architecture for training
+
+# convolution block with BatchNormalization
+def ConvBlock(in_channels, out_channels, pool=False):
+    layers = [nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+             nn.BatchNorm2d(out_channels),
+             nn.ReLU(inplace=True)]
+    if pool:
+        layers.append(nn.MaxPool2d(4))
+    return nn.Sequential(*layers)
+
+class ResNet9(ImageClassificationBase):
+    def __init__(self, in_channels, num_diseases):
+        super().__init__()
+        
+        self.conv1 = ConvBlock(in_channels, 64)
+        self.conv2 = ConvBlock(64, 128, pool=True) # out_dim : 128 x 64 x 64 
+        self.res1 = nn.Sequential(ConvBlock(128, 128), ConvBlock(128, 128))
+        
+        self.conv3 = ConvBlock(128, 256, pool=True) # out_dim : 256 x 16 x 16
+        self.conv4 = ConvBlock(256, 512, pool=True) # out_dim : 512 x 4 x 44
+        self.res2 = nn.Sequential(ConvBlock(512, 512), ConvBlock(512, 512))
+        
+        self.classifier = nn.Sequential(nn.MaxPool2d(4),
+                                       nn.Flatten(),
+                                       nn.Linear(512, num_diseases))
+        
+    def forward(self, xb): # xb is the loaded batch
+        out = self.conv1(xb)
+        out = self.conv2(out)
+        out = self.res1(out) + out
+        out = self.conv3(out)
+        out = self.conv4(out)
+        out = self.res2(out) + out
+        out = self.classifier(out)
+        return out 
+
+# Example usage
+model_path = "ML_Model/pretrained_model/plant-disease-model-complete2.pth"
+
+# Assuming ResNet9 is defined in your FastAPI script
+model =ResNet9(3, 38)
+
+# Load the entire model
+model = torch.load(model_path, map_location=torch.device('cpu'))
+
+model.eval()
+
 CLASS_NAMES=['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rust', 'Apple___healthy', 
              'Blueberry___healthy', 'Cherry_(including_sour)___Powdery_mildew', 'Cherry_(including_sour)___healthy',
              'Corn_(maize)___Cercospora_leaf_spot Gray_leaf_spot', 'Corn_(maize)___Common_rust_', 
@@ -24,10 +127,7 @@ CLASS_NAMES=['Apple___Apple_scab', 'Apple___Black_rot', 'Apple___Cedar_apple_rus
              'Tomato___Target_Spot', 'Tomato___Tomato_Yellow_Leaf_Curl_Virus', 'Tomato___Tomato_mosaic_virus', 
              'Tomato___healthy']
 
-def read_file_as_image(data) -> np.ndarray:
-    image = np.array(Image.open(BytesIO(data)))
-    image=cv2.resize(image, (70,70))
-    return image
+
 @app.get("/ping")
 async def ping():
     return "hello"
@@ -36,27 +136,34 @@ async def predict(
     file: UploadFile = File(...)
 ):
     
-    image = read_file_as_image(await file.read())
-    print(image)
-    image = image.astype("float32")
-    image = image/255.0
-   
-    image = np.reshape(image, [1, 70, 70, 3])
-    print("working or not")
-    predictions = MODEL.predict(image)
-    print(predictions, "/n")
+    image = await file.read()
+    image = Image.open(BytesIO(image)).convert('RGB')
+    print(" before : ", image)
+    image=image.resize((256,256))
+
+    image= np.array(image)
+    image= np.reshape(image, (256, 256, 3))
+    image = Image.fromarray(image)
+    print(" after : ", image)
+    transform = transforms.ToTensor()
+    img=transform
+    img_tensor = transform(image)
     
-    predicted_class_index = np.argmax(predictions)
-    predicted_class = CLASS_NAMES[predicted_class_index]
-    print("predicted class : ", predicted_class, " at index : ", predicted_class_index)
+    image=img_tensor.unsqueeze(0)
+    yb = model(image)
+    # Pick index with highest probability
+    _, preds  = torch.max(yb, dim=1)
+    # with torch.no_grad():
+    #     output = model(image)
+  
+    # preds  = torch.max(output, dim=1)
+    print("pred is : ",preds[0].item())
+    predicted_class =  CLASS_NAMES[preds[0].item()]
 
     
-    confidence = np.max(predictions)
     return {
         'class': predicted_class,
-        'confidence': float(confidence)
     }
 
-    print(img_batch)
 if __name__ == "__main__":
     uvicorn.run(app,host='localhost',port=8000)
