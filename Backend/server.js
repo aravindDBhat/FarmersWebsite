abbcf1906f255c44ff3cdf7e9bfeb6d479608f0d
aravindDbhat <aravindbhat88@gmai.com>
2024-07-22 16:31:12 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,25 @@ const Dotenv = require("dotenv");
 const userService = require("./Routes/user.routes");
 Dotenv.config();
 const app = Express();
+const allowedOrigins = [
+  "https://farmers-website-rbwm.vercel.app",
+  "https://farmers-website-gfxg.vercel.app",
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
 app.use(Express.json());
 app.use(bodyparser.json());
 app.use("/files", Express.static("files"));
@@ -15,7 +34,6 @@ app.use(
     extended: true,
   })
 );
-app.use(cors());
 mongoose
   .connect(process.env.MONGO_DB_URL, {
     useNewUrlParser: true,
